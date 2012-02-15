fd4e6ae09041f926a899644a40c26e1af49d8ce3
Shuo Zhang <szhang@unca.edu>
2012-02-15 15:05:13 -0800

    Disappearing input box and set up counter line

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -1,83 +1,72 @@
 /**
  * This is the entry point for our JavaScript program
+ * Project 3
  */
 function main() {
     //your code goes here
-//    alert("hello world!");
-	var count = 0;
-//	var object_array = new Array();
-	var object_array = [];
+	$("#search_button").click(function() {
+		$("#search_button").fadeOut(2000, function() {
 
-    //your tasks
+		});
+		$("#term").fadeOut(2000, function() {
 
-    //1. Create a spotter and get it to insert tweets into the DOM
-	
-	var s = new Spotter("twitter.search", 
-						{q:"Pokemon", period:120},
-						{buffer:true, bufferTimeout:750}
-						);
-						
-	s.register(function(tweet) {
-		count = count + 1;
-		var color;
-
-		//2. Add profile images (tweet.profile_image_url)
-		var profile_image = "<img src=' "+tweet.profile_image_url+" ' />";	
-		
-		if (count%2 === 0) {
-			color = "red";
-		} else {
-			color = "blue";
-		}
-		
-//		$("p:even").css("background-color", "#E8E8E8");
-//		$("p:odd").css("background-color", "#C0C0C0");
-				
-		//3. Make the tweets occur so the most recent are at the top				
-		//4. Make the tweets slide down		
-
-		var object = $("<p class='"+color+"'>" + profile_image + tweet.text+ "</p>");
-				
-		object.hide();
-		$("#tweets").prepend(object);
-		object.slideDown();
-		
-		object_array.push(object);
-		
-		if (object_array.length > 10) {
-			var object_to_remove = object_array.shift();
-			object_to_remove.fadeOut(2000, function() {
-				object_to_remove.remove();
-			});
-		}	
-		
-//		$("#tweets p:gt(9)").fadeOut(200, function() {
-//			$("#tweets p:gt(9)").remove();		
-//		});
-
-    //5. Alternate the colors or the background of the tweets
-										
+		});
 		
 	});
 	
-	s.start();
-	
-	// Jeison value (key: value, period:seconds),
-	// 				(Buffer gives one at a time, instead of array)
+	$("#search_button").click(startSearch);
 
-	// function tweet appended as id
+	function startSearch() {
+		var counted = 10;
 	
-	// start spotter
+		var counting = $("<p>How many times this has appeared: " + counted + "</p>");
+		$("#tweets").append(counting);	
 	
+		var love_count = 0;
 	
-	
-	
-	
-	
-    //6. Show a maximum of 10 tweets at a time (remove old tweets from the dom)
-	//   Array (hint)
+	//    alert("hello world!");
+		var count = 0;
+	//	var object_array = new Array();
+		var object_array = [];
 
+		//your tasks
 
+		//1. Create a spotter and get it to insert tweets into the DOM
+	
+		var s = new Spotter("twitter.search", 
+							{q:"$('#term')", period:120},
+							{buffer:true, bufferTimeout:750}
+							);
+						
+						
+		s.register(function(tweet) {
+			count = count + 1;
+			var color;
+
+			//2. Add profile images (tweet.profile_image_url)
+			var profile_image = "<img src=' "+tweet.profile_image_url+" ' />";	
+		
+			var object = $("<p class='"+color+"'>" + profile_image + tweet.text+ "</p>");
+				
+			object.hide();
+			$("#tweets").prepend(object);
+			object.slideDown();
+		
+			// check to see if tweet has the word love
+			// if the tweet contains love. increment love_count
+		
+			if (tweet.text.match(/love/i) ) {
+				$("#tweets").prepend(object);
+			}
+							
+		
+		});
+	
+		s.start();
+	}	
+	
 }
 
-main();
+$(document).ready(function() {
+	main();
+});
