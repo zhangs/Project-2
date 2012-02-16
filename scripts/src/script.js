6725da24da516893a45bc1ca441b396e5a2e42e1
Shuo Zhang <szhang@unca.edu>
2012-02-16 14:14:02 -0800

    Format set-up; results from search appearing

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -3,6 +3,9 @@
  * Project 3
  */
 function main() {
+
+	var occurance = 0;
+
     //your code goes here
 	$("#search_button").click(function() {
 		$("#search_button").fadeOut(2000, function() {
@@ -16,51 +19,57 @@ function main() {
 	
 	$("#search_button").click(startSearch);
 
-	function startSearch() {
-		var counted = 10;
-	
-		var counting = $("<p>How many times this has appeared: " + counted + "</p>");
-		$("#tweets").append(counting);	
-	
-		var love_count = 0;
-	
+	function startSearch() {		
 	//    alert("hello world!");
-		var count = 0;
+	//	var count = 0;
 	//	var object_array = new Array();
-		var object_array = [];
+	//	var object_array = [];
 
 		//your tasks
 
 		//1. Create a spotter and get it to insert tweets into the DOM
+
+
 		var searched = $("#term").val();
-	
+
+		var count_text = $("<p>How many times " + searched + " occured: </p>");
+		$("#results").append(count_text);	
+		
 		var s = new Spotter("twitter.search", 
-							{q:searched, period:120},
+							{q:"Nintendo", period:120},
 							{buffer:true, bufferTimeout:750}
 							);
 						
 						
 		s.register(function(tweet) {
-			count = count + 1;
-			var color;
+		//	count = count + 1;
+		//	var color;
+			
 
 			//2. Add profile images (tweet.profile_image_url)
-			var profile_image = "<img src=' "+tweet.profile_image_url+" ' />";	
+			//var profile_image = "<img src=' "+tweet.profile_image_url+" ' />";	
 		
-			var object = $("<p class='"+color+"'>" + profile_image + tweet.text+ "</p>");
+			//var object = $("<p class='"+color+"'>" + profile_image + tweet.text+ "</p>");
+			$('#tweets').empty();					
 				
-			object.hide();
-			$("#tweets").prepend(object);
-			object.slideDown();
-		
-			// check to see if tweet has the word love
-			// if the tweet contains love. increment love_count
-		
-			if (tweet.text.match(/love/i) ) {
-				$("#tweets").prepend(object);
+			var counting = $("<p>" + occurance + "</p>");
+			$("#tweets").append(counting);			
+
+			if (tweet.text.match(searched) ) {
+				occurance = occurance + 1;		
 			}
 							
+
+			
+				
+//			object.hide();
+//			$("#tweets").prepend(object);
+//			object.slideDown();
+			
+			//var counted = 10;
 		
+			// check to see if tweet has the word love
+			// if the tweet contains love. increment love_count	
 		});
 	
 		s.start();
